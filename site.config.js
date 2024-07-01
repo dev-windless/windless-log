f8a63121e4b1cc16a27c2b4838219ac9965866cb
dev-windless <107724629+dev-windless@users.noreply.github.com>
2024-07-01 22:00:45 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,6 +10,12 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
+    projects: [
+    {
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
+  ],
   
   // blog setting (required)
   blog: {
